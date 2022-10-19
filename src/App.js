57045cebd6140bdb56cc294a1adb862e5538ba80
AbdulRehman <likeam99@gmail.com>
2022-10-19 09:08:52 +0500

    Dropdwon css

--- src/App.js
+++ src/App.js
@@ -20,10 +20,25 @@ const items = [
   }
 ];
 
+const options = [
+  {
+    label: 'The Color Red',
+    value: 'red'
+  },
+  {
+    label: 'The Color Green',
+    value: 'green'
+  },
+  {
+    label: 'The Color Blue',
+    value: 'blue'
+  }
+]
+
 const App = () => {
   return (
     <div>
-      <Dropdown  />
+      <Dropdown  options={options}  />
     </div>
   )
 }
--- src/components/Dropdown.js
+++ src/components/Dropdown.js
@@ -1,8 +1,25 @@
 import React from 'react'
 
-const Dropdown = () => {
+const Dropdown = ({options}) => {
+
+    const renderedOptions = options.map((option) =>{
+        return(
+            <div key={option.value} className="item">
+                {option.label}
+            </div>
+        );
+    }); 
     return (
-        <div>Dropdown</div>
+        <div className="ui form">
+            <div className="field">
+                <label className='label'>Select a Color</label>
+                <div className="ui selection dropdwon visibla active">
+                    <i className="dropdwon icon"></i>
+                    <div className="text">Select Color</div>
+                    <div className="menu visible transition">{renderedOptions}</div>
+                </div>
+            </div>
+        </div>
     )
 }
 
