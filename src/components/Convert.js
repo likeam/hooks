bf54d8efae2df32179b2b2af359917a0a33955d6
AbdulRehman <likeam99@gmail.com>
2022-10-30 04:01:59 +0500

    Debouncing  Translation

--- src/components/Convert.js
+++ src/components/Convert.js
@@ -1,20 +1,46 @@
-import React, { useEffect } from 'react'
+import React, { useEffect, useState, useSyncExternalStore } from 'react'
 import axios from 'axios';
 
 const Convert = ({language, text}) => {
 
+    const [translated, setTranslated] = useState('');
+    const [debouncedText, setDebouncedText] = useState(text);
+
+    useEffect(() => {
+        const timerId = setTimeout(() => {
+            setDebouncedText(text);
+        }, 500);
+
+        return () => {
+            clearTimeout(timerId);
+        };
+    }, [text]);
+
     useEffect(() =>{
-        axios.post('https://translation.googleapis.com/language/translate/v2', {}, {
-            params: {
-                q: text,
-                target: language.value,
-            }
-        })
-    }, [language, text]);
+
+        const doTranslation = async () => {
+
+            const { data } = await axios.post('https://translation.googleapis.com/language/translate/v2',
+            {}, 
+            {
+                params: {
+                    q: debouncedText,
+                    target: language.value,
+                    key: 'AIzaSyCHUCmpR7cT_yDFHC98CZJy2LTms-IwDlM'
+                }
+            });
+            setTranslated(data.data.translations[0].translatedText);
+        };
+
+        doTranslation();
+
+    }, [language, debouncedText]);
 
 
     return (
-        <div>Convert</div>
+        <div>
+            <h1 className="ui header">{translated}</h1>
+        </div>
     )
 }
 
